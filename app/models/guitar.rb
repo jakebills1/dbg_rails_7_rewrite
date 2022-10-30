c58e695c5a2e33e39de2406ade98dfa1c34cad2d
Jake Bills <jakebills1@gmail.com>
2022-10-30 09:26:10 -0600

    adds action text; runs install; adds has_rich_text_for_description

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -2,6 +2,7 @@ class Guitar < ApplicationRecord
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
+  has_rich_text :description
   validates :name, :price, :description, :year, presence: true
   validates :name, uniqueness: true
   validates :year, inclusion: { in: 1900..Date.today.year }
