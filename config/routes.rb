e8deb7fbe79debf4696cc560c8b379d72e5fd546
Jake Bills <jakebills1@gmail.com>
2022-11-02 21:55:10 -0600

    basic gallery page

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/', to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # namespace :api do
-  #   resources :guitars
-  # end
-  resources :guitars
+  get '/gallery', to: 'static_pages#gallery'
+  resources :guitars # move to namespace, as this will be under admin panel
   scope :active_storage, module: :active_storage, as: :active_storage do
     resources :attachments, only: [:destroy]
   end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
     it "returns http success" do
-      get "/static_pages/home"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /contact" do
     it "returns http success" do
-      get "/static_pages/contact"
+      get "/contact"
       expect(response).to have_http_status(:success)
     end
   end
