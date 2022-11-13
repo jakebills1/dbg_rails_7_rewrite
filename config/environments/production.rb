f5a6ce835f25c580ad3272ced18657bc87e7ea3b
Jake Bills <jakebills1@gmail.com>
2022-11-13 14:34:16 -0700

    uses cloudfront for asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_controller.asset_host = ENV.fetch('CLOUDFRONT_URL')
 end
