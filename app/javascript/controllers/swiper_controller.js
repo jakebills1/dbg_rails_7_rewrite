d47a0163623a3a0154d65e18acb1314762e5f91b
Jake Bills <jakebills1@gmail.com>
2022-11-13 09:12:41 -0700

    basic lightbox using swiper

---
+++ app/javascript/controllers/swiper_controller.js
@@ -0,0 +1,32 @@
+import Swiper from 'swiper';
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ["container", "next", "prev"];
+  static values = { activeKey: String };
+
+  connect() {
+    this.swiper = new Swiper(this.containerTarget, {
+      navigation: {
+        nextEl: this.nextTarget,
+        prevEl: this.prevTarget
+      }
+    });
+  }
+
+  navigate(event) {
+    event.preventDefault();
+
+    const { key } = event.detail;
+
+    this.activeKeyValue = key;
+  }
+
+  activeKeyValueChanged() {
+    const activeSlide = this.swiper?.slides.findIndex(
+      slide => slide.dataset.key == this.activeKeyValue
+    );
+
+    this.swiper?.slideTo(activeSlide, 0);
+  }
+}
