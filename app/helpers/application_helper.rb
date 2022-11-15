e9665d6ddcee7190e961036f7d38255adcbfec9d
Jake Bills <jakebills1@gmail.com>
2022-11-15 08:30:06 -0700

    fixes error with cdn_for

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def cdn_for(file)
-    Rails.env.production? ? "#{ENV['S3_CDN_URL']}/#{file.key}" : url_for(file)
+    Rails.env.production? ? "#{ENV['S3_CDN_URL']}#{file.key}" : url_for(file)
   end
 end
